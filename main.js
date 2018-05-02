30146653f91b8232d3e24bed1af44a1258d41a1c
alcaloke <alcaloke@gmail.com>
2018-05-02 09:34:18 +0100

    javascript updated

--- main.js
+++ main.js
@@ -1,9 +1,43 @@
-let myHeading = document.querySelector('h2');
-myHeading.textContent = 'by an electric dragon';
-myHeading.onclick = function() {
-  alert('you have been electrified!');
+// onlclick; ALERT
+// let myHeading = document.querySelector('h2');
+// myHeading.textContent = 'by an electric dragon';
+// myHeading.onclick = function() {
+//   alert('you have been electrified!');
+// }
+
+// change images
+let myImage = document.querySelector('img');
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'assets/color.jpg') {
+    myImage.setAttribute('src', 'assets/organ.jpg')
+  } else {
+    myImage.setAttribute('src', 'assets/color.jpg')
+  }
 }
 
+// using prompt to enter, store & display data
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter you name');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Hello there, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Hello there, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+// p5 canvas
 function setup(){
   createCanvas(600, 300);
 }
